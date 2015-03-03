a3f48156c052cbb3590dce643659faba13353cf3
Akul <akurnya@gmail.com>
2015-03-03 21:34:22 +0530

    created a rough draft file for adding dir paths containing py modules to sys path

---
+++ src/bridge/mgmtscripts/path_insert.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import yaml
+import contextlib
+
+@contextlib.contextmanager
+def yaml_load(yml_stream):
+    try:
+        f = yaml.load(yml_stream)
+        yield f
+    except AttributeError, e:
+        print "Error %s" %(e)
+    finally:
+        del f
+        
+
+
+def get_root_path(parent_path= os.getcwd(), root_path='cloudroutes-service'):    
+    """
+    Gets the root directory path given current working directory for
+    execution iff current directory is a subdirectory of root directory.
+    """
+    parent_path, cur_dir = os.path.split(os.path.abspath
+    (parent_path))
+    if cur_dir == root_path: return parent_path, cur_dir
+    else: return get_root_path(parent_path=parent_path)
+
+
+def path_insert(yml_path, path_keys, dry_run = 1):
+    """
+    Gets the correct target root path.
+    Iterates over paths given as yaml configuration keys
+    and adds them to the sys.path if legitimate.
+    """
+    with yaml_load(open(yml_path)) as f:
+        parent_path, root_dir_name = get_root_path()
+        for key in path_keys:
+            paths = [root_dir_name] + f[key].split(',')
+            target_path = os.path.join(parent_path, *paths)
+            if os.path.exists(target_path):
+                if dry_run:
+                    print "Dry run path %s" %(target_path)
+                else:
+                    sys.path.insert(1,target_path)
